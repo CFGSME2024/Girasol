87715571626eaf1dd394737e7df7594ad982e206
Iker Clemente <ikercp0@gmail.com>
2023-11-23 17:10:13 +0100

    Display

--- Arduino/006_Display_Direccio/006_Display_Direccio.ino
+++ Arduino/006_Display_Direccio/006_Display_Direccio.ino
@@ -2,25 +2,48 @@
 **                                                                               **
 **                           Iker Clemente Palacios                              **
 **                                   23/11/23                                    **
-**                         Programa complet del girasol                          **
+**                          Trobar direcció del display                          **
 **                                                                               **
 **********************************************************************************/
+
 //********** Includes *************************************************************
+#include <Wire.h>
 
 //********** Variables ************************************************************
 
 //********** Setup ****************************************************************
 void setup() 
 {
-  
-
+  Wire.begin();
+  Serial.begin(9600);
 }
 
 //********** Loop *****************************************************************
 void loop() 
 {
-  
+ byte codigoDeRespuesta, direccion;
+ int dispositivosEncontrados = 0;
+ Serial.println("\n\nBuscando dispositivos...");
 
+  for (direccion = 1; direccion < 127; direccion++ ) {
+    Wire.beginTransmission(direccion);
+    codigoDeRespuesta = Wire.endTransmission();
+
+    if (codigoDeRespuesta == 0) {
+      Serial.print("Dispositivo encontrado en la dirección 0x");
+      if (direccion < 16)
+        Serial.print("0");
+      Serial.print(direccion, HEX);
+       dispositivosEncontrados++;
+    }else if (codigoDeRespuesta == 4) {
+      Serial.print("Error desconocido en la dirección 0x");
+      if (direccion < 16)
+        Serial.print("0");
+      Serial.println(direccion, HEX);
+    }
+  }
+  if (dispositivosEncontrados == 0) Serial.println("No se encontró ningún dispositivo");
+  delay(1000);
 }
 
 //********** Funcions *************************************************************
